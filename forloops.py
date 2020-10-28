3ea70dba1c7136ee57390fb891d189e2a6428f32
Andrew Green Vellon <73195898+AndrewGreen96@users.noreply.github.com>
2020-10-28 23:50:39 +0000

    Update forloops.py

--- forloops.py
+++ forloops.py
@@ -5,24 +5,24 @@
 # Creating the list
 pizzas = []
 
-# Populating the list
-pizzas.append('pepperoni')
-pizzas.append('four seasons')
-pizzas.append('fromagi')
-pizzas.append('margarita')
-pizzas.append('meat feast')
-pizzas.append('bbq chicken')
+# # Populating the list
+# pizzas.append('pepperoni')
+# pizzas.append('four seasons')
+# pizzas.append('fromagi')
+# pizzas.append('margarita')
+# pizzas.append('meat feast')
+# pizzas.append('bbq chicken')
 
-# Printing the list to check it's been populated correctly
-print(pizzas)
+# # Printing the list to check it's been populated correctly
+# print(pizzas)
 
-# Creating the for loop
-for pizza in pizzas:
-    print(f'The {pizza.title()} is one of my favourite pizzas and would recommend it to my friends.\n')
-print(f'I really love pizza but my favourite pizza is the {pizzas[4].title()}!')
+# # Creating the for loop
+# for pizza in pizzas:
+#     print(f'The {pizza.title()} is one of my favourite pizzas and would recommend it to my friends.\n')
+# print(f'I really love pizza but my favourite pizza is the {pizzas[4].title()}!')
 
-# 4.2 Animals
-# Add the names of some animals to a list and then finish off by writing what they all have in common
+# # 4.2 Animals
+# # Add the names of some animals to a list and then finish off by writing what they all have in common
 
 animals = []
 
